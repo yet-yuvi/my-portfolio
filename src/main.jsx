92f8d8bddcb8c850bbbc2ddea8af12803271e343
Jubayar Ahamad <eng.jubayar@gmail.com>
2026-09-20 02:23:46 +0600

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { StrictMode } from 'react';
+
+import './index.css';
+
+const rootElement = document.getElementById('root');
+
+createRoot(rootElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
