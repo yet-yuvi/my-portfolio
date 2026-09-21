c829554ec94bcd4b3ead9e97d1678c3f7a9ab3fc
jubayar <eng.jubayar@gmail.com>
2026-09-22 04:55:23 +0600

    feat: Add Social.jsx component and fix layout App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import Image from './components/Image';
 import Bio from './components/Bio';
+import Social from './components/Social';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-300 gap-4">
+    <div className="flex items-center justify-center h-screen bg-gray-200 gap-4">
       <Image />
-      <Bio />
-      {/* social links */}
+      <div className="flex flex-col gap-6">
+        <Bio />
+        <Social />
+      </div>
     </div>
   );
 }
