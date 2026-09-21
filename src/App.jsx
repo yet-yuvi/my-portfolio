fac342e8dc6524dd154b978b0ec6b58738020e10
jubayar <eng.jubayar@gmail.com>
2026-09-21 23:58:38 +0600

    feat: Add Bio.jsx component on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Image from './components/Image';
+import Bio from './components/Bio';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-300">
+    <div className="flex items-center justify-center h-screen bg-gray-300 gap-4">
       <Image />
-      {/* bio */}
+      <Bio />
       {/* social links */}
     </div>
   );
