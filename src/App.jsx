d0aa418036126fbf060c7f6aac0cfc4da82c74d7
jubayar <eng.jubayar@gmail.com>
2026-09-23 04:08:06 +0600

    feat: Add Hero.jsx component
    style: Format code

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import Image from './components/Image';
-import Bio from './components/Bio';
-import Social from './components/Social';
+import Hero from './components/Hero';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-200 gap-4">
-      <Image />
-      <div className="flex flex-col gap-6">
-        <Bio />
-        <Social />
-      </div>
+    <div className="gap-4 p-8 bg-gray-200 flex flex-col items-center justify-center h-full">
+      <Hero />
     </div>
   );
 }
--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,32 +1,32 @@
 function Social() {
   return (
-    <ul className='flex gap-4'>
+    <ul className="flex gap-4">
       <li>
         <a
-          className='text-sky-500 hover:underline'
-          href='https://www.linkedin.com/in/jubayar-ahamad-find/'
-          target='_blank'
-          rel='noopener noreferrer'
+          className="text-sky-500 hover:underline"
+          href="https://www.linkedin.com/in/jubayar-ahamad-find/"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           LinkedIn
         </a>
       </li>
       <li>
         <a
-          className='text-sky-500 hover:underline'
-          href='https://github.com/yet-yuvi'
-          target='_blank'
-          rel='noopener noreferrer'
+          className="text-sky-500 hover:underline"
+          href="https://github.com/yet-yuvi"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           GitHub
         </a>
       </li>
       <li>
         <a
-          className='text-sky-500 hover:underline'
-          href='https://jubayar-portfolio.netlify.app/'
-          target='_blank'
-          rel='noopener noreferrer'
+          className="text-sky-500 hover:underline"
+          href="https://jubayar-portfolio.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Profile
         </a>
